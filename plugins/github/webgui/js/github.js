195f21d287fbf1974f990e05b159218dff760c98
intjftw <anett.fekete@ericsson.com>
2021-12-04 16:54:13 +0100

    Project hosting service navigator window appears in webgui.

--- plugins/github/webgui/js/github.js
+++ plugins/github/webgui/js/github.js
@@ -11,10 +11,43 @@ require([
 function (Tooltip, ObjectStoreModel, declare, Memory, Observable, topic,
     HtmlTree, model, viewHandler) {
 
+    //model.addService('githubservice', 'GitHubService', GitHubServiceClient);
+
     var ProjectHostingServiceNavigator = declare(HtmlTree, {
         constructor : function () {
             var that = this;
+
             this._data = [];
+
+            this._store = new Observable(new Memory({
+                data : this._data,
+                getChildren : function (node) {
+                    return node.getChildren ? node.getChildren(node) : [];
+                }
+            }));
+
+            var dataModel = new ObjectStoreModel({
+                store : that._store,
+                query : { id : 'root' },
+                mayHaveChildren : function (node) {
+                    return node.hasChildren;
+                }
+            });
+
+            //--- Tree ---//
+            // ezt a gitNavigator.js-ből vettem, nem kell így hagyni
+            this._data.push({
+                id          : 'root',
+                name        : 'List of repositories',
+                cssClass    : 'icon-list',
+                hasChildren : true,
+                getChildren : function () {
+                    return that._store.query({ parent : 'root' });
+                }
+            });
+
+            this.set('model', dataModel);
+            this.set('openOnClick', false);
         }
     });
 
@@ -24,8 +57,8 @@ function (Tooltip, ObjectStoreModel, declare, Memory, Observable, topic,
     });
 
     viewHandler.registerModule(navigator, {
-        type : viewHandler.moduleType.Accordion
+        type : viewHandler.moduleType.Accordion,
+        priority : 50
     });
-
 
 });
